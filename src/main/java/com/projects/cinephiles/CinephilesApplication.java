0ab6b99dd2e79224d546b684410b6f49bc11764b
Rohan More <rohan110620@gmail.com>
2024-08-26 10:01:05 +0530

    This is first commit of cinephiles backend

--- src/main/java/com/projects/cinephiles/CinephilesApplication.java
+++ src/main/java/com/projects/cinephiles/CinephilesApplication.java
@@ -10,6 +10,7 @@ public class CinephilesApplication {
 	public static void main(String[] args) {
 
 		System.out.println("Hello");
+		System.out.println("Rohan");
 
 		SpringApplication.run(CinephilesApplication.class, args);
 	}
