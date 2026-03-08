43fb607f43413b27aade7ac5581b1ca5bb95c5e1
Rohan M <rohan110620@gmail.com>
2026-03-08 17:50:42 +0530

    added movie repo for data

--- src/main/java/com/projects/cinephiles/Repo/MovieRepo.java
+++ src/main/java/com/projects/cinephiles/Repo/MovieRepo.java
@@ -1,12 +1,14 @@
 package com.projects.cinephiles.Repo;
 
 import com.projects.cinephiles.models.Movie;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -25,4 +27,6 @@ public interface MovieRepo extends JpaRepository<Movie,Long> {
 
 
     List<Movie> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+
+    Page<Movie> findByReleaseDateAfter(LocalDate date, Pageable pageable);
 }
