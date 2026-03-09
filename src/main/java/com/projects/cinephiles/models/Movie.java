d142100fd5ea9e34bf2ebef4b5669ee059a13645
Rohan M <rohan110620@gmail.com>
2026-03-09 19:47:58 +0530

    changed made

--- src/main/java/com/projects/cinephiles/models/Movie.java
+++ src/main/java/com/projects/cinephiles/models/Movie.java
@@ -58,10 +58,14 @@ public class Movie {
     @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CrewMember> crew;
 
+    @Column(length = 1000)
     private String poster;
+
+    @Column(length = 1000)
     private String banner;
 
     @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL , orphanRemoval = true)
+    @Column(length = 1000)
     private List<Trailers> trailers;
 
     private LocalDate releaseDate;
