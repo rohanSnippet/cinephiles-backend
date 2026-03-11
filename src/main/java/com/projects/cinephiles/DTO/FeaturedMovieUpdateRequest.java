b0448507864943d6a199f181f39b1773722bca76
Rohan M <rohan110620@gmail.com>
2026-03-11 20:27:26 +0530

    changed

--- src/main/java/com/projects/cinephiles/DTO/FeaturedMovieUpdateRequest.java
+++ src/main/java/com/projects/cinephiles/DTO/FeaturedMovieUpdateRequest.java
@@ -1,2 +1,14 @@
-package com.projects.cinephiles.DTO;public class FeaturedMovieUpdateRequest {
+package com.projects.cinephiles.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class FeaturedMovieUpdateRequest {
+    private String region;
+    private List<Long> movieIds;
 }
