7320aab64a621478e27ac80e2a3abae0cda256d6
Rohan M <rohan110620@gmail.com>
2026-03-14 10:56:25 +0530

    changes made to payment gateway

--- src/main/java/com/projects/cinephiles/Repo/BookingRepository.java
+++ src/main/java/com/projects/cinephiles/Repo/BookingRepository.java
@@ -2,6 +2,8 @@ package com.projects.cinephiles.Repo;
 
 import com.projects.cinephiles.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
     Optional<Booking> findByBookingID(String bookingID);
+
+    @Query("SELECT b FROM Booking b WHERE b.show.id = :showId AND b.seatsIds LIKE concat('%', :seatId, '%')")
+    Optional<Booking> findByShowIdAndSeatIdPart(@Param("showId") Long showId, @Param("seatId") String seatId);
 }
