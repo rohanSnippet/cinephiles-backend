be72174ebfd1b72cf81299320791989bf81e49ca
Rohan M <rohan110620@gmail.com>
2026-04-05 13:00:17 +0530

    comment dto added

--- src/main/java/com/projects/cinephiles/DTO/CommentRequestDto.java
+++ src/main/java/com/projects/cinephiles/DTO/CommentRequestDto.java
@@ -1,2 +1,11 @@
-package com.projects.cinephiles.DTO;public class CommentRequestDto {
+package com.projects.cinephiles.DTO;
+
+import lombok.Data;
+
+@Data
+public class CommentRequestDto {
+    private Long movieId;
+    private String content;
+    private Long parentId; // Null if it's a top-level comment
+    private String repliedToUsername;
 }
