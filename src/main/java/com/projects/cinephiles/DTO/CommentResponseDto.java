817c849e15ce8a5278b7f14d8a66a3bfc03e9fea
Rohan M <rohan110620@gmail.com>
2026-04-05 13:00:36 +0530

    comment response dto added

--- src/main/java/com/projects/cinephiles/DTO/CommentResponseDto.java
+++ src/main/java/com/projects/cinephiles/DTO/CommentResponseDto.java
@@ -1,2 +1,17 @@
-package com.projects.cinephiles.DTO;public class CommentResponseDto {
+package com.projects.cinephiles.DTO;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class CommentResponseDto {
+    private Long id;
+    private String authorUsername;
+    private String fullName;
+    private String profile;
+    private String content;
+    private LocalDateTime createdAt;
+    private int upvotes;
+    private String repliedToUsername;
+    private long replyCount; // Tells React whether to render a "View Replies" button
 }
